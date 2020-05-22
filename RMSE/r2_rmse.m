b5bdf220d9556466a399a0a59c4ad6ca6d1f16b4
Functional Advanced Neuroimaging (FAN) Lab @ University of California, Davis <64869063+fanlab-ucdavis@users.noreply.github.com>
2020-05-21 17:50:25 -0700

    Update r2_rmse.m
    Added description for each input value and output value

--- RMSE/r2_rmse.m
+++ RMSE/r2_rmse.m
@@ -1,8 +1,16 @@
 function [rms, vox] = r2_rmse(T2, TE, magTE0, map)
 
-% 9:18pm May, 14 2020 
+% 5:45pm May, 21 2020 
 
-% produce a 4D prediction using magTE0 and map produced by the ...
+% T2 is the original image as a 4D voxel
+% TE is the echo times for T2
+% magTE0 are the estimated starting values for the mono-exponential curve
+% map is the R2 values taken from the nonlin_fit.m
+
+% rms is the root mean square difference for T2 and vox
+% vox is nonlin_fitted image
+
+% produces a 4D prediction using magTE0 and map produced by the ...
 % r2_nonlin_fit.m function
     [x, y] = size(TE);
     dim = size(magTE0);
